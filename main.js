1eb3ad05958ef596f211f1e6a585b3f339ed5074
Ashley Almonte <ashleyalmonte14@gmail.com>
2019-05-16 10:04:19 -0400

    workshop

---
+++ main.js
@@ -0,0 +1,16 @@
+  class Classic {
+  constructor(art_url)
+  this.frame = 'black-border'
+  this.art_url ? art_url: 'deafult_url'
+  this.frame = 'black-border'
+  this.art_url
+}
+
+  render() {
+  let el = document.createElement('div')
+  el.style.width = '100px'
+  el.style.height = '100px'
+  el.style.backgroundColor = 'black'
+  document.body.appendChild(el)
+
+}
